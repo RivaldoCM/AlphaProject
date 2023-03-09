73395b107ec85c4554329aa91a6d96cfa5b14dae
RivaldoCM <rivaldocardoso2010@hotmail.com>
2023-03-08 21:51:10 -0300

    iniciando tela de configurações do usuario.

--- src/screens/Settings/index.js
+++ src/screens/Settings/index.js
@@ -1,10 +1,59 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+
+import { Profile, BoxImage, BoxInfos, Button, BoxIcon, BoxText } from "./style";
+
+import Icon from "react-native-vector-icons/Feather";
 
 export function Settings(){
     return(
         <View>
-            <Text>Config</Text>
+            <View>
+                <TouchableOpacity>
+                    <Profile>
+                        <BoxImage>
+
+                        </BoxImage>
+                        <BoxInfos>
+                            <Text>Rivaldo</Text>
+                        </BoxInfos>
+                    </Profile>
+                </TouchableOpacity>
+            </View>
+            <View>
+                <View>
+                    <Button>
+                        <BoxIcon>
+                            <Icon name="lock" size={20} />
+                        </BoxIcon>
+                        <BoxText>
+                            <Text>Conta</Text>
+                            <Text>Conta</Text>
+                        </BoxText>
+                    </Button>
+                    <Button>
+                        <BoxIcon>
+                            <Icon name="shield" size={20} />
+                        </BoxIcon>
+                        <Text>Privacidade</Text>
+                    </Button>
+                    <Button>
+                        <BoxIcon>
+                            <Icon name="lock" size={20} />
+                        </BoxIcon>
+                        <Text>Temas</Text>
+                    </Button>
+                    <Button>
+                        <BoxIcon>
+                            <Icon name="log-out" size={20} />
+                        </BoxIcon>
+                        <BoxText>
+                            <Text>Logout</Text>
+                            <Text>Clique aqui para sair de sua conta</Text>
+                        </BoxText>
+                    </Button>
+                </View>
+            </View>
         </View>
     )
 }
---
+++ src/screens/Settings/style.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const Profile = styled.View`
+    flex-direction: row;
+    height: 120px;
+`;
+
+export const BoxImage = styled.View`
+    width: 30%;
+    height: 100%;
+    background-color: aliceblue;
+`;
+
+export const BoxInfos = styled.View`
+    justify-content: center;
+    width: 70%;
+    height: 100%;
+    padding: 10px;
+`;
+
+export const Button = styled.TouchableOpacity`
+    flex-direction: row;
+    align-items: center;
+    height: 70px;
+    margin: 5px;
+`;
+
+export const BoxIcon = styled.View`
+    align-items: center;
+    justify-content: center;
+    width: 10%;
+    height: 100%;
+`;
+
+export const BoxText = styled.View`
+    justify-content: center;
+    width: 90%;
+    height: 100%;
+`;
